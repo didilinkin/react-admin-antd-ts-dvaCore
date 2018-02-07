ac7321d00f5d7507ac4dd916f3424a513b6f41c4
yanxiaodi <929213769@qq.com>
2018-02-07 23:02:03 +0800

    :bug: Set Router 4

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,32 +8,58 @@
 /* tslint:disable */
 
 import * as React from 'react'
-import { renderRoutes } from 'react-router-config'
+// import { renderRoutes } from 'react-router-config'
 import {
   BrowserRouter as Router,
-  Switch,
+  // Switch,
+  Route,
+  Link,
 } from 'react-router-dom'
 
-import configRoutes from './configRoutes'
+// import configRoutes from './configRoutes'
 
-const RouteView = ({ route }: any) => (
-  <Switch> { renderRoutes(route.routes) } </Switch>
+// const RouteView = ({ route }: any) => (
+//   <Switch> { renderRoutes(route.routes) } </Switch>
+// )
+
+// const rootRoutes = [{
+//   component: RouteView,
+//   routes: configRoutes,
+// }]
+
+// class RootRouter extends React.Component {
+//   // getChildContext
+//   render () {
+//     return (
+//       <Router>
+//         { renderRoutes(rootRoutes) }
+//       </Router>
+//     )
+//   }
+// }
+
+// export default RootRouter
+
+const ParamsExample = () => (
+  <Router>
+    <div>
+      <h2>账号</h2>
+      <ul>
+        <li><Link to="/react-router">React Router</Link></li>
+        <li><Link to="/leoashin">LeoAshin</Link></li>
+        <li><Link to="/justjavac">justjavac</Link></li>
+        <li><Link to="/reacttraining">React Training</Link></li>
+      </ul>
+
+      <Route path="/:id" component={Child}/>
+    </div>
+  </Router>
 )
 
-const rootRoutes = [{
-  component: RouteView,
-  routes: configRoutes,
-}]
+const Child = ({ match }: any) => (
+  <div>
+    <h3>ID: {match.params.id}</h3>
+  </div>
+)
 
-class RootRouter extends React.Component {
-  // getChildContext
-  render () {
-    return (
-      <Router>
-        { renderRoutes(rootRoutes) }
-      </Router>
-    )
-  }
-}
-
-export default RootRouter
+export default ParamsExample
